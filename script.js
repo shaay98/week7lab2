dd6448892cd27bd2bb74bf48d902b20b0fe82346
shaay98 <nrichardson567@icloud.com>
2025-01-17 02:51:32 +0000

    lab2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict"
+
+let products = [
+{ id: 1, name: 'Echo Dot', price: 29.99, quantity: 1 },
+{ id: 2, name: 'Fire TV Stick', price: 39.99, quantity: 2 },
+{ id: 3, name: 'Kindle Paperwhite', price: 119.99, quantity: 1 },
+{ id: 4, name: 'Echo Show 5', price: 89.99, quantity: 0 },
+{ id: 5, name: 'Fire Tablet', price: 49.99, quantity: 3 },
+{ id: 6, name: 'Ring Video Doorbell', price: 99.99, quantity: 1 }
+];
+
+
